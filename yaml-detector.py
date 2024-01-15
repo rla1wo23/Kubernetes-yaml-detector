eef0ad280491c008bad759a63fc4b970ea149f65
lenntil2 <87885343+lenntil2@users.noreply.github.com>
2024-01-15 15:50:35 +0900

    Improve layout
    Use code block to print result
    Use flexbox layout
    Use wide layout

--- yaml-detector.py
+++ yaml-detector.py
@@ -3,25 +3,45 @@
 import streamlit as st
 from yamlfix import fix_code
 
-TEXT = {
-    "HEADER": "Cloudwave YAML 문법검사기",
-    "NOTICE": """\
-사용법: From Text Box에 원하는 텍스트를 넣고, Ctrl+Enter를 넣으면 아래 To 결과박스에 결과가 나옵니다.
-만약 잘 되지 않는다거나, 기타 문의사항은 https://open.kakao.com/o/sHwnai3f 여기로 연락 부탁드려요.
-EC2 t3.micro 인스턴스로 배포중인데, 너무 약한 서버라서 트래픽 조금만 쌓여도 터집니다...\
-    """,
-}
 
-st.header(TEXT["HEADER"])
-st.text(TEXT["NOTICE"])
+def main():
+    TEXT = {
+        "HEADER": "Cloudwave YAML 문법검사기",
+        "NOTICE": """\
+    사용법: From Text Box에 원하는 텍스트를 넣고, Ctrl+Enter를 넣으면 아래 To 결과박스에 결과가 나옵니다.
+    만약 잘 되지 않는다거나, 기타 문의사항은 https://open.kakao.com/o/sHwnai3f 여기로 연락 부탁드려요.
+    EC2 t3.micro 인스턴스로 배포중인데, 너무 약한 서버라서 트래픽 조금만 쌓여도 터집니다...\
+        """,
+    }
+
+    TEXT_AREA_HEIGHT = 480
+    st.set_page_config(layout="wide")
+
+    st.header(TEXT["HEADER"])
+    st.text(TEXT["NOTICE"])
+
+    col1, col2, col3 = st.columns([3, 1, 3])
+
+    with col1:
+        st.markdown("### From:")
+        q = st.text_area(
+            "From text area",
+            height=TEXT_AREA_HEIGHT,
+            placeholder="포맷팅 할 YAML 텍스트를 입력하세요.",
+            label_visibility="collapsed"
+        )
+
+    with col2:
+        btn = st.button("Format ➡️")
+
+    with col3:
+        result = "Waiting for input..."
+        if q or btn:
+            result = fix_code(q)
+            print(f"Result: {result}")
+        st.markdown("### To:")
+        st.code(result, language="YAML")
 
 
-result = ""
-q = st.text_area("From")
-btn = st.button("제출")
-
-if q or btn:
-    result = fix_code(q)
-    print(f"Result: {result}")
-
-st.text_area("To", value=result)
+if __name__ == "__main__":
+    main()
